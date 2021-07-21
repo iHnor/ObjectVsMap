5b36f26f6fe9d92f905196edc209a61a6df23c14
Ihor <mixnoiv@gmail.com>
2021-07-21 23:18:41 +0300

    create print function

--- wordFrequency.js
+++ wordFrequency.js
@@ -7,20 +7,30 @@ function wordFrequency (text){
 
         if (map.has(lowerText) == false) {
             map.set(lowerText, 1);
-            console.log(text[i]);                
+            // console.log(text[i]);                
         }
         else {
             valueKey = map.get(lowerText)
             map.set(lowerText, valueKey + 1);
         }
-        console.log(map);
+        // console.log(map);
     }
+    return map;
     // map.set(text[1], 1);
     // console.log(map.has(text[1]));
 
 }
 
+function printRezultat(printText){
+    for (const [key, value] of printText){
+        console.log(key,' - ', value);
+    }
+}
+
 someText = 'Little red fox likes red box Red box is big'
 let splitText = someText.split(' ');
 // console.log(splitText);
-let rezult = wordFrequency(splitText); 
+let rezult = wordFrequency(splitText);
+printRezultat(rezult);
+// console.log(rezult);
+
