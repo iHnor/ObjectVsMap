34ca8d1b5649ae18779ba007a6a78b586a85ee23
Ihor <mixnoiv@gmail.com>
2021-07-21 22:53:01 +0300

    complete word frequency function

--- wordFrequency.js
+++ wordFrequency.js
@@ -1,10 +1,26 @@
 function wordFrequency (text){
     let map = new Map();
-    map.set(text[1], 1);
-    console.log(map.has(text[1]));    
+
+    for (let i = 0; i < text.length; i++){ 
+        // console.log(map.has(text[i]));
+        lowerText = text[i].toLowerCase();
+
+        if (map.has(lowerText) == false) {
+            map.set(lowerText, 1);
+            console.log(text[i]);                
+        }
+        else {
+            valueKey = map.get(lowerText)
+            map.set(lowerText, valueKey + 1);
+        }
+        console.log(map);
+    }
+    // map.set(text[1], 1);
+    // console.log(map.has(text[1]));
+
 }
 
 someText = 'Little red fox likes red box Red box is big'
 let splitText = someText.split(' ');
 // console.log(splitText);
-let rezult = wordFrequency(someText); 
+let rezult = wordFrequency(splitText); 
