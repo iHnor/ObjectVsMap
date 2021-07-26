49d239461168b555d249435d028b6240c9ded6b0
Ihor Mykhno <igormykhno@gmail.com>
2021-07-26 10:00:16 +0300

    Fixing test.js for new file employeeBirthWithCSV

--- employeeBirthdays.test.js
+++ employeeBirthdays.test.js
@@ -1,10 +1,14 @@
 const moment = require('moment');
-import * as emp from './employeeBirthdays.js';
+import * as emp from './employeeBirthWithCSV.js';
 
 let employeesBirthdays = [
     {name: 'Ваня Иванов', date: moment([2000, 9, 9])},
     {name: 'Ваня Иванов', date: moment([2000, 9, 9])}
 ]
+
+let words = ["лет", "год", "года"];
+let todayData = new Date();
+let totalYear = todayData.getFullYear();
 
 describe('Employee birthday', () => {
     describe('Change the number date to the letter date when', () => {
@@ -30,33 +34,33 @@ describe('Employee birthday', () => {
 
             it('when age equal 1', () => {
                 let number = 1;
-                expect(emp.funcPluralization(number)).toBe('год');
+                expect(emp.funcPluralization(number, words)).toBe('1 год');
             });
 
             it('when age equal 3', () => {
                 let number = 3;
-                expect(emp.funcPluralization(number)).toBe('года');
+                expect(emp.funcPluralization(number, words)).toBe('3 года');
             });
 
             it('when age equal 6', () => {
                 let number = 6;
-                expect(emp.funcPluralization(number)).toBe('лет');
+                expect(emp.funcPluralization(number, words)).toBe('6 лет');
             });
 
             it('when age equal 15', () => {
                 let number = 15;
-                expect(emp.funcPluralization(number)).toBe('лет');
+                expect(emp.funcPluralization(number, words)).toBe('15 лет');
             });
 
             it('when age equal 21', () => {
                 let number = 21;
-                expect(emp.funcPluralization(number)).toBe('год');
+                expect(emp.funcPluralization(number, words)).toBe('21 год');
             });
         });
         it('number form', () => {
             
             let date = employeesBirthdays[0].date;
-            expect(emp.funcGetAge(date)).toBe('21 год');
+            expect(emp.funcGetAge(date, totalYear)).toBe('21 год');
         });
     });
 });
